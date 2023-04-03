d1c01deeb0dbe15c1fb3a866ba1294ef43aba237
canknbr <cankanbur1@gmail.com>
2023-04-03 14:41:02 +0300

    Initial Commit

---
+++ SpriteKit Animation/SpriteKit_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_AnimationApp.swift
+//  SpriteKit Animation
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
